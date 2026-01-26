7d32075f64eb17c980aa4c7036cfa97ed2523dcb
nsportsman <nathan.sportsman@praetorian.com>
2026-01-26 05:50:00 -0600

    feat(sctp): add SCTPScan and update ScanTargets

--- pkg/scan/scan_api.go
+++ pkg/scan/scan_api.go
@@ -20,6 +20,21 @@ import (
 	"github.com/praetorian-inc/nerva/pkg/plugins"
 )
 
+// SCTPScan performs SCTP scanning on all targets.
+func SCTPScan(targets []plugins.Target, config Config) ([]plugins.Service, error) {
+	var results []plugins.Service
+	for _, target := range targets {
+		result, err := config.SCTPScanTarget(target)
+		if err == nil && result != nil {
+			results = append(results, *result)
+		}
+		if config.Verbose && err != nil {
+			log.Printf("%s\n", err)
+		}
+	}
+	return results, nil
+}
+
 func UDPScan(targets []plugins.Target, config Config) ([]plugins.Service, error) {
 	var results []plugins.Service
 	for _, target := range targets {
@@ -39,6 +54,9 @@ func UDPScan(targets []plugins.Target, config Config) ([]plugins.Service, error)
 func ScanTargets(targets []plugins.Target, config Config) ([]plugins.Service, error) {
 	var results []plugins.Service
 
+	if config.SCTP {
+		return SCTPScan(targets, config)
+	}
 	if config.UDP {
 		return UDPScan(targets, config)
 	}
